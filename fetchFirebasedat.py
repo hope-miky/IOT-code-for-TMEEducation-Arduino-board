bacf63ddeefc838be3145c2775dc1699f29a309e
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-08 22:05:40 +0300

    fetching from and to firebase completed

---
+++ fetchFirebasedat.py
@@ -0,0 +1,35 @@
+from firebase import firebase
+from time import sleep
+
+class FireB:
+    def __init__(self):
+        self.firebase = firebase.FirebaseApplication('https://iotfortme.firebaseio.com', None)
+        
+    
+    def getTemprature(self):
+        return self .firebase.get('/Temp', None)
+    
+    def setTempature(self, temp):
+        self.firebase.put('/', '/Temp', temp)
+    
+    def getLight(self):
+        return self.firebase.get('/Light', None)
+        
+    def setLight(self, light):
+        self.firebase.put('/', '/Light', light)
+
+    def getTheHoledata(self):
+        return self.firebase.get('/', None)
+
+    def updateValue(self, value):
+        self.firebase.put('/', '/Btnstate', value)
+
+
+
+fireb = FireB()
+
+print(fireb.getLight())
+fireb.updateValue(0)
+
+
+
