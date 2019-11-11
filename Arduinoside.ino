60955575228a5a990eeffd4dc77e8aac2481707e
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-12 00:12:02 +0300

    Arduino code updated

--- Arduinoside.ino
+++ Arduinoside.ino
@@ -2,11 +2,14 @@
 int temp = 0;
 
 void setup() {
+  // put your setup code here, to run once:
 pinMode(tempin, INPUT);
 Serial.begin(9600);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 temp = analogRead(tempin) ;//* 0.125 - 22.0;
 Serial.println(temp);
+delay(1000);
 }
