03ad62e182f9193a8964d50aa8df5e70b75d9a93
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-12 21:47:26 +0300

    commented out seril class

--- serialconnector.py
+++ serialconnector.py
@@ -1,16 +1,24 @@
+'''
+This Class handles the serial communication with the Arduino board 
+Made by TME Education Ethiopia 
+'''
+
+#Importing neccessary libraries 
 import serial
 from time import sleep
-import asyncio
+
 
 class serialClass:
     def __init__(self):
-        self.boundrate = 9600
+        #Constructor for the class
+        self.boundrate = 9600               #Bound rate which is defined in the Arduino code 
         self.val = ""
-        self.ser = serial.Serial('/dev/ttyACM0',self.boundrate, timeout=2)
+        self.ser = serial.Serial('/dev/ttyACM0',self.boundrate, timeout=2)      #Defining an Object for the serial communication with a certain characteristics 
 
 
     def getSerialData(self):
-        self.val = self.ser.readline().decode('utf-8', 'ignore')
-        return self.val.rstrip()
+        #Get serial data from the Arduino 
+        self.val = self.ser.readline().decode('utf-8', 'ignore')        #Decode the recieved values to strings (utf-8)
+        return self.val.rstrip()                                        #Remove the strip and return the value
 
 
